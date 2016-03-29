f5a02e6c9758d9ed8449c2341108fbab6439b3d1
brooke hurford and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-29 10:01:16 -0700

    Show article tile on index page, pulling data from firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'bustle',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://bustle19.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
