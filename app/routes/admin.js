7864d6fb83a776511b1004c12bfc1b411f8660ce
brooke hurford and Joseph Karasek <brookebhurford@gmail.com>
2016-03-29 10:42:01 -0700

    add delete article function to admin page

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
+  },
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('admin');
+    }
   }
 });
